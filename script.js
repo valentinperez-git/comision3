a8e3c8cdd94cd1203389d037ae2c024755911f4f
valentinoRiccie <valentinperezf15@gmail.com>
2023-10-26 17:11:12 -0300

    feat: Se agrega funciones basicas al proyecto

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ const nombre3 = "nene"
 console.log(nombre3)
 
 alert("Bienvenidos");
+
+const sum = (a, b) => a + b;
+const res = (a, b) => a - b;
+const div = (a, b) => a / b;
+const mul = (a, b) => a * b;
+
+
